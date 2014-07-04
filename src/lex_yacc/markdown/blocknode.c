9882f9ccf86521bdd7a24f3175b9e16e8f76ddcf
hudamin <hudamin@baidu.com>
2014-07-04 19:26:36 +0800

    develop-markdown: add blocknodestack.c and update blocknode.c and .h

--- src/lex_yacc/markdown/blocknode.c
+++ src/lex_yacc/markdown/blocknode.c
@@ -6,6 +6,69 @@
 
 t_blocknode *node_list = NULL, *node_current = NULL;
 
+/* inner function prototypes */
+void pop_higher_level_blocknode(int level);
+char *blocknode_glue(t_blocknode *top, t_blocknode *current);
+char *blocknode_output(t_blocknode *node);
+
+
+
+
+
+
+
+void pop_higher_level_blocknode(level){
+    t_blocknode *item = blocknode_top_stack();
+
+    while( item
+        && item->indent_level >= level){
+        blocknode_pop_stack();
+        /*
+        printf("pop tag: %s, level %d\n", get_tag_type(item->tag), item->indent_level);
+        */
+        item = blocknode_top_stack();
+    }
+}
+
+char *blocknode_glue(t_blocknode *top, t_blocknode *current){
+    return "";
+}
+
+char *blocknode_output(t_blocknode *node){
+    switch(node->tag){
+        case TAG_NULL: printf(""); break;
+
+        case TAG_P: printf("%s", node->ops[0]); break;
+        case TAG_OL:
+        case TAG_UL: printf("<li>%s", node->ops[0]); break;
+        case TAG_PRE: printf("%s", node->ops[1]); break;
+
+        case TAG_QUOTE: printf(""); break;
+        case TAG_BLANK: printf(""); break;
+
+        case TAG_H: printf(""); break;
+        case TAG_EOF: printf(""); break;
+
+        case TAG_QUOTE_P: printf(""); break;
+        case TAG_QUOTE_UL: printf(""); break;
+        case TAG_QUOTE_OL: printf(""); break;
+        case TAG_QUOTE_PRE: printf(""); break;
+        case TAG_QUOTE_BLANK: printf(""); break;
+        case TAG_QUOTE_H: printf(""); break;
+
+        case TAG_INDENT_P: printf(""); break;
+        case TAG_INDENT_UL: printf(""); break;
+        case TAG_INDENT_OL: printf(""); break;
+        case TAG_INDENT_PRE: printf(""); break;
+    }
+
+    return "";
+}
+
+
+
+
+
 t_blocknode *blocknode_create(t_tag tag, int level, int nops, ...){
     va_list args;
     t_blocknode *p;
@@ -52,27 +115,33 @@ char *blocknode_show(t_blocknode *node){
 
     switch(node -> nops){
         case 1:
-            format = "%s\t, [%s]";
+            format = "%-*s (%d):\t\t[%s]";
             s = str_format(
                 format
+                , 15
                 , get_tag_type(node->tag)
+                , node->indent_level
                 , node->ops[0]
             );
             break;
         case 2:
-            format = "%s\t, [%s], [%s]";
+            format = "%-*s (%d):\t\t[%s], [%s]";
             s = str_format(
                 format
+                , 15
                 , get_tag_type(node->tag)
+                , node->indent_level
                 , node->ops[0]
                 , node->ops[1]
             );
             break;
         case 3:
-            format = "%s\t, [%s], [%s], [%s]";
+            format = "%-*s (%d):\t\t[%s], [%s], [%s]";
             s = str_format(
                 format
+                , 15
                 , get_tag_type(node->tag)
+                , node->indent_level
                 , node->ops[0]
                 , node->ops[1]
                 , node->ops[2]
@@ -80,10 +149,12 @@ char *blocknode_show(t_blocknode *node){
             break;
 
         default:
-            format = "%s";
+            format = "%-*s (%d)";
             s = str_format(
                 format
+                , 15
                 , get_tag_type(node->tag)
+                , node->indent_level
             );
             break;
     }
@@ -101,7 +172,14 @@ void blocklist_parse(){
     t_blocknode *p = node_list;
 
     while(p){
+
+        pop_higher_level_blocknode(p->indent_level);
+        blocknode_push_stack(p);
+        blocknode_output(p);
+
+        /*
         blocknode_show(p);
+        */
         p = p->next;
     }
 
